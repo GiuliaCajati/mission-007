910d7eed8b47bd0bf716d0cfd695ce75a2f1e8e6
giuliacajati <giuliacajati@gmail.com>
2021-05-06 15:31:22 -0400

    modified tests

--- spec/text_editor_spec.rb
+++ spec/text_editor_spec.rb
@@ -24,53 +24,54 @@ RSpec.describe TextEditor do
 
         context 'given the insert operation,' do
           it 'inserts the string to the buffer, starting at the given index' do
-            @editor.perform(:insert, 'if you would make the right choices', 0)
-            @editor.perform(:insert, 'an exception to it, ', 0)
-            @editor.perform(:insert, 'You might be ', 0)
-            expect(@editor.buffer).to eq('You might be an exception to it, if you would make the right choices')
+            @editor.perform(:insert, 'Hello', 0)
+            @editor.perform(:insert, '!', -1)
+            @editor.perform(:insert, ' world', 5)
+            expect(@editor.buffer).to eq('Hello world!')
           end
         end
       end 
 
       context 'given the cut operation,' do
         it 'removes the characters from the buffet starting at the given index' do
-          @editor.buffer = 'You might be an exception to it, if you would make the right choices'
-          @editor.perform(:cut, 5, 41)
-          expect(@editor.buffer).to eq('You might be an exception to it, if you wmake the right choices')
+          @editor.buffer = 'Mission Accomplished!'
+          @editor.perform(:cut, 8, 0)
+          expect(@editor.buffer).to eq('Accomplished!')
         end
         it 'adds the removed characters to the end of the clipboard' do
-          @editor.buffer = 'You might be an exception to it, if you would make the right choices'
-          @editor.perform(:cut, 5, 41)
-          expect(@editor.clipboard[-1]).to eq('ould ')
+          @editor.buffer = 'Mission Accomplished!'
+          @editor.perform(:cut, 3, -5)
+          expect(@editor.clipboard[-1]).to eq('she')
         end
       end
     
 
       context 'given the duplicate operation,' do
         it 'adds the buffer at the given index' do
-          @editor.buffer = 'Your mission, shif ould you wmake the ight chooses  to acceptit'
-          @editor.perform(:duplicate, 1, 13, -3)
-          expect(@editor.buffer).to eq('Your mission, shif ould you wmake the ight chooses  to accept it')
+          @editor.buffer = 'Light The Fuse '
+          @editor.perform(:duplicate, 5, 0, -1)
+          expect(@editor.buffer).to eq('Light The Fuse Light')
         end
         it 'adds the copied characters to the end of the clipboard' do
-          @editor.buffer = 'Your mission, shif ould you wmake the ight chooses  to acceptit'
-          @editor.perform(:duplicate, 1, 13, -3)
-          expect(@editor.clipboard[-1]).to eq(' ')
+          @editor.clipboard = ['Make', 'It', 'Count']
+          @editor.buffer = 'Red Light!'
+          @editor.perform(:duplicate, 5, 4, 2)
+          expect(@editor.clipboard).to eq(['Make', 'It', 'Count', 'Light'])
         end
       end
 
       context 'given the paste operation,' do
         it 'inserts the string, that was last added to the clipboard, on to the buffer at the given index' do
-          @editor.buffer = 'You misght be an exionit, if you wmake the right choicoses ac'
-          @editor.clipboard = ["ould ", " to ", "cept", "s"]
-          @editor.perform(:paste, 3)
-          expect(@editor.buffer).to eq('Yous misght be an exionit, if you wmake the right choicoses ac')
+          @editor.buffer = 'The Countdown Is Helping.'
+          @editor.clipboard = ['one','two','three', 'Not ']
+          @editor.perform(:paste, -9)
+          expect(@editor.buffer).to eq('The Countdown Is Not Helping.')
         end
         it 'removes the string that was last added to the clipboard' do
-          @editor.buffer = 'You misght be an exionit, if you wmake the right choicoses ac'
-          @editor.clipboard = ["ould ", " to ", "cept", "s"]
-          @editor.perform(:paste, 3)
-          expect(@editor.clipboard).to eq(["ould ", " to ", "cept"])
+          @editor.buffer = 'The Countdown Is Helping.'
+          @editor.clipboard = ['one','two','three']
+          @editor.perform(:paste, 0)
+          expect(@editor.clipboard).to eq(['one','two'])
         end
       end
 
